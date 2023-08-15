7aa2d3df91a12213566b5e803aa89ea919bc45d9
yaojun <940334249@qq.com>
2023-08-15 08:43:21 +0800

    format: use clang_format

--- docs/QA.md
+++ docs/QA.md
@@ -1,6 +1,1 @@
-## the difference for flush() and fsync()
-## the key_dir didn't persistent to disk
-## the index and key_dir need reload when the server restart
-## how to use the index?
-## char data not equals to string data
-## curl http://0.0.0.0:18080/yaojun
+## What is the difference between flush() and fsync() ?
--- src/include/server.h
+++ src/include/server.h
@@ -1,59 +1,60 @@
 #include <cstddef>
+#include <ctime>
+#include <fcntl.h>
 #include <filesystem>
 #include <fstream>
+#include <iostream>
 #include <mutex>
 #include <shared_mutex>
-#include <string>
-#include <unordered_map>
-#include <utility>
-#include <string>
-#include <fcntl.h>
-#include <ctime>
-#include <filesystem>
-#include <fstream>
-#include <unordered_set>
-#include <iostream>
-#include <unistd.h>
-#include <sys/types.h>    
-#include <sys/stat.h>    
-#include <fcntl.h>
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
+#include <string>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
 #include <vector>
 namespace yesdb {
-    class Entry {
-    public:
-        Entry()=default;
-        Entry(int fd, int offset, int size_);
-        int fd_;
-        int offset_;
-        int size_;
-    };
+class Entry {
 
-    class Yesdb{
-    public:
-        Yesdb(std::string filename);
-        ~Yesdb();
-        bool Open();
-        bool Close();
-        bool Put(const std::string key, const std::string value);
-        bool Get(const std::string key, std::string &value);
-        bool Flush();
-        bool Sync();
-        bool Compress(const std::string org_data, std::vector<char> &cmpr_data,int &cmpr_size);
-        bool Decompress(std::vector<char> &cmpr_data, std::string &decmpr_data, const int cmpr_size, const int org_size);
-    private:
-        std::string filename_;
-        int fd_;
-        int offset_;
-        std::string data_;
-        std::unordered_map<std::string, Entry> key_dir;
-        std::shared_mutex mutex;
-        inline static const char *TOMBSTONE = "YESDB_TOMBSTONE_VALUE";
+public:
+  Entry() = default;
+  Entry(int fd, int offset, int size_);
+  int fd_;
+  int offset_;
+  int size_;
+};
 
-        bool ReadData(Entry entry, std::string &value);
-        bool Serialize();
-        bool Deserialize();
+class Yesdb {
+public:
+  Yesdb(std::string filename);
+  ~Yesdb();
+  bool Open();
+  bool Close();
 
-    };
+  bool Put(const std::string key, const std::string value);
+  bool Get(const std::string key, std::string &value);
+  bool Flush();
+
+  bool Sync();
+  bool Compress(const std::string org_data, std::vector<char> &cmpr_data,
+                int &cmpr_size);
+  bool Decompress(std::vector<char> &cmpr_data, std::string &decmpr_data,
+                  const int cmpr_size, const int org_size);
+
+private:
+  std::string filename_;
+  int fd_;
+  int offset_;
+  std::string data_;
+  std::unordered_map<std::string, Entry> key_dir;
+  std::shared_mutex mutex;
+  inline static const char *TOMBSTONE = "YESDB_TOMBSTONE_VALUE";
+
+  bool ReadData(Entry entry, std::string &value);
+  bool Serialize();
+  bool Deserialize();
+};
 } // namespace yesdb
